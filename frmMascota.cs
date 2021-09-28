70c875794987c78212dfa8e94645e60518bebf59
Axel Moore <83082913+axelmoore1@users.noreply.github.com>
2021-09-28 19:01:15 -0300

    Add files via upload

---
+++ frmMascota.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+
+
+namespace TUP_PI_Recu_Mascotas
+{
+    public partial class frmMascota : Form
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        public frmMascota()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void frmMascota_Load(object sender, EventArgs e)
+        {
+            conexion = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Veterinaria;Integrated Security=True");
+            cargarcombo();
+            cargarLista();
+            controles(true);
+        }
+
+        private void controles(bool value)
+        {
+            btnNuevo.Enabled = value;
+            btnSalir.Enabled = value;
+            btnGrabar.Enabled = !value;
+            txtCodigo.Enabled = !value;
+            txtNombre.Enabled = !value;
+            cboEspecie.Enabled = !value;
+            rbtHembra.Enabled = !value;
+            rbtMacho.Enabled = !value;
+            dtpFechaNacimiento.Enabled = !value;
+            lstMascotas.Enabled = !value;
+                
+        }
+
+        private void cargarLista()
+        {
+            try
+            {
+                conexion.Open();
+                DataTable tabla = new DataTable();
+                comando = new SqlCommand();
+                comando.CommandText = "SELECT (str(codigo) + ' -- ' + nombre) as datos, codigo FROM Mascotas";
+                comando.Connection = conexion;
+                tabla.Load(comando.ExecuteReader());
+                conexion.Close();
+
+                lstMascotas.DataSource = tabla;
+                lstMascotas.ValueMember = "codigo";
+                lstMascotas.DisplayMember = "datos";
+            }
+            catch (Exception) { }
+        }
+
+        private void cargarcombo()
+        {
+            try
+            {
+                conexion.Open();
+                DataTable tabla = new DataTable();
+                comando = new SqlCommand();
+                comando.CommandText = "SELECT * FROM Especies";
+                comando.Connection = conexion;
+                tabla.Load(comando.ExecuteReader());
+                conexion.Close();
+
+                cboEspecie.DataSource = tabla;
+                cboEspecie.ValueMember = tabla.Columns[0].ColumnName;
+                cboEspecie.DisplayMember = tabla.Columns[1].ColumnName;
+            }
+            catch (Exception) 
+            {
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            try { 
+                if (String.IsNullOrEmpty(txtCodigo.Text.Trim()))
+                {
+                    MessageBox.Show("Campo obligatorio!");
+                    txtCodigo.Focus();
+                    return;
+                }
+                if (txtNombre.Text.Equals(""))
+                {
+                    MessageBox.Show("Debe ingresar un nombre");
+                    txtNombre.Focus();
+                    return;
+                }
+                if(cboEspecie.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Debe seleccionar una mascota");
+                    cboEspecie.Focus();
+                    return;
+                }
+                int codigo;
+                try
+                {
+                    codigo = Convert.ToInt32(txtCodigo.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Codigo Incorrecto!");
+                    return;
+                }
+            
+                Mascota nueva = new Mascota();
+                nueva.pCodigo = Convert.ToInt32(txtCodigo.Text);
+                nueva.pNombre = txtNombre.Text;
+                nueva.pEspecie = cboEspecie.SelectedIndex;
+                if (rbtMacho.Checked)
+                    nueva.pSexo = 1;
+                else
+                    nueva.pSexo = 2;
+                nueva.pFechaNacimiento = dtpFechaNacimiento.Value;
+
+                conexion = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Veterinaria;Integrated Security=True");
+           
+                conexion.Open();
+                comando = new SqlCommand();
+                comando.Connection = conexion;
+                comando.CommandText = "INSERT INTO Mascotas (codigo,nombre,especie,sexo,fechaNacimiento)" +
+                    "VALUES (@codigo, @nombre, @especie, @sexo, @fechaNacimiento)";
+                comando.Parameters.AddWithValue("codigo", nueva.pCodigo);
+                comando.Parameters.AddWithValue("nombre", nueva.pNombre);
+                comando.Parameters.AddWithValue("especie", nueva.pEspecie);
+                comando.Parameters.AddWithValue("sexo", nueva.pSexo);
+                comando.Parameters.AddWithValue("fechaNacimiento", nueva.pFechaNacimiento);
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("Mascota cargada correctamente");
+                cargarLista();
+                controles(true);
+                limpiarCampos();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo cargar la mascota ");
+            }
+        }
+
+        private void limpiarCampos()
+        {
+            txtCodigo.Text = txtNombre.Text = string.Empty;
+            rbtHembra.Checked = false;
+            rbtMacho.Checked = false;
+            cboEspecie.SelectedIndex = -1;
+            dtpFechaNacimiento.Value = DateTime.Now;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            controles(false);
+            txtCodigo.Focus();
+            limpiarCampos();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Seguro desea salir la aplicación ?",
+                "SALIR", MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2) == 
+                DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
+
---
+++ frmMascota.designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TUP_PI_Recu_Mascotas
+{
+    partial class frmMascota
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEspecie = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cboEspecie = new System.Windows.Forms.ComboBox();
+            this.rbtMacho = new System.Windows.Forms.RadioButton();
+            this.rbtHembra = new System.Windows.Forms.RadioButton();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.lstMascotas = new System.Windows.Forms.ListBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(103, 44);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(52, 17);
+            this.lblCodigo.TabIndex = 10;
+            this.lblCodigo.Text = "Código";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(97, 100);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 11;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblEspecie
+            // 
+            this.lblEspecie.AutoSize = true;
+            this.lblEspecie.Location = new System.Drawing.Point(96, 155);
+            this.lblEspecie.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEspecie.Name = "lblEspecie";
+            this.lblEspecie.Size = new System.Drawing.Size(58, 17);
+            this.lblEspecie.TabIndex = 12;
+            this.lblEspecie.Text = "Especie";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(115, 210);
+            this.lblSexo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(39, 17);
+            this.lblSexo.TabIndex = 13;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(32, 266);
+            this.lblFechaNacimiento.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(121, 17);
+            this.lblFechaNacimiento.TabIndex = 14;
+            this.lblFechaNacimiento.Text = "Fecha Nacimiento";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(213, 43);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(75, 22);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(213, 97);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(216, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // cboEspecie
+            // 
+            this.cboEspecie.FormattingEnabled = true;
+            this.cboEspecie.Location = new System.Drawing.Point(213, 151);
+            this.cboEspecie.Margin = new System.Windows.Forms.Padding(4);
+            this.cboEspecie.Name = "cboEspecie";
+            this.cboEspecie.Size = new System.Drawing.Size(216, 24);
+            this.cboEspecie.TabIndex = 2;
+            // 
+            // rbtMacho
+            // 
+            this.rbtMacho.AutoSize = true;
+            this.rbtMacho.Location = new System.Drawing.Point(213, 208);
+            this.rbtMacho.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtMacho.Name = "rbtMacho";
+            this.rbtMacho.Size = new System.Drawing.Size(92, 21);
+            this.rbtMacho.TabIndex = 3;
+            this.rbtMacho.TabStop = true;
+            this.rbtMacho.Text = "1 - Macho";
+            this.rbtMacho.UseVisualStyleBackColor = true;
+            // 
+            // rbtHembra
+            // 
+            this.rbtHembra.AutoSize = true;
+            this.rbtHembra.Location = new System.Drawing.Point(331, 208);
+            this.rbtHembra.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtHembra.Name = "rbtHembra";
+            this.rbtHembra.Size = new System.Drawing.Size(100, 21);
+            this.rbtHembra.TabIndex = 4;
+            this.rbtHembra.TabStop = true;
+            this.rbtHembra.Text = "2 - Hembra";
+            this.rbtHembra.UseVisualStyleBackColor = true;
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(213, 258);
+            this.dtpFechaNacimiento.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(128, 22);
+            this.dtpFechaNacimiento.TabIndex = 5;
+            // 
+            // lstMascotas
+            // 
+            this.lstMascotas.FormattingEnabled = true;
+            this.lstMascotas.ItemHeight = 16;
+            this.lstMascotas.Location = new System.Drawing.Point(493, 37);
+            this.lstMascotas.Margin = new System.Windows.Forms.Padding(4);
+            this.lstMascotas.Name = "lstMascotas";
+            this.lstMascotas.Size = new System.Drawing.Size(220, 244);
+            this.lstMascotas.TabIndex = 9;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(56, 322);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(100, 28);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Enabled = false;
+            this.btnGrabar.Location = new System.Drawing.Point(331, 322);
+            this.btnGrabar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(100, 28);
+            this.btnGrabar.TabIndex = 7;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(612, 322);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(100, 28);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmMascota
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 387);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.lstMascotas);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.rbtHembra);
+            this.Controls.Add(this.rbtMacho);
+            this.Controls.Add(this.cboEspecie);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblEspecie);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCodigo);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMascota";
+            this.Text = "Mascotas";
+            this.Load += new System.EventHandler(this.frmMascota_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEspecie;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cboEspecie;
+        private System.Windows.Forms.RadioButton rbtMacho;
+        private System.Windows.Forms.RadioButton rbtHembra;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.ListBox lstMascotas;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
